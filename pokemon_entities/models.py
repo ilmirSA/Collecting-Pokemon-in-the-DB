7b7576e02881812dc47bbddf82180168d2077650
ilmirSA <ilmir531@gmail.com>
2025-02-04 16:58:59 +0300

    create Pokemon model.

---
+++ pokemon_entities/models.py
@@ -0,0 +1,6 @@
+from django.db import models  # noqa F401
+
+
+class Pokemon(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    title = models.CharField(max_length=200)
