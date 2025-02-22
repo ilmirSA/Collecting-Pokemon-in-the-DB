c5cb7a97931eb985938b50a059b187bdf494777e
ilmirSA <71828619+ilmirSA@users.noreply.github.com>
2025-02-22 13:39:04 +0300

    replaced the names in Russian

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,17 +5,18 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True)
-    description = models.TextField(blank=True)
+    title = models.CharField(verbose_name="Имя покемона",max_length=200)
+    title_en = models.CharField(verbose_name="Имя покемона на английском", max_length=200, blank=True)
+    title_jp = models.CharField(verbose_name="Имя покемона на японском", max_length=200, blank=True)
+    avatar = models.ImageField(verbose_name="Картинка", upload_to='avatars/', blank=True)
+    description = models.TextField(verbose_name="Описание",blank=True)
     previous_evolution = models.ForeignKey(
         "self",
         related_name='next_evolutions',
         on_delete=models.SET_NULL,
         blank=True,
-        null=True
+        null=True,
+        verbose_name="Предыдущая эволюция"
     )
 
     def __str__(self):
@@ -26,13 +27,12 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField(verbose_name="Широта")
     lon = models.FloatField(verbose_name="Долгота")
-    appeared_at = models.DateTimeField(default=datetime.datetime.now())
-    disappeared_at = models.DateTimeField(default=datetime.datetime.now())
-    level = models.IntegerField(default=1)
-    hp = models.IntegerField(default=100)
-    attack = models.IntegerField(default=5)
-    protection = models.IntegerField(default=3)
-    endurance = models.IntegerField(default=2)
-
+    appeared_at = models.DateTimeField(verbose_name="Время появления",default=datetime.datetime.now())
+    disappeared_at = models.DateTimeField(verbose_name="Время исчезновения", default=datetime.datetime.now())
+    level = models.IntegerField(verbose_name="Уровень", default=1)
+    hp = models.IntegerField(verbose_name="Здоровье",default=100)
+    attack = models.IntegerField(verbose_name="Атака",default=5)
+    protection = models.IntegerField(verbose_name="Защита",default=3)
+    endurance = models.IntegerField(verbose_name="Выносливость",default=2)
     def __str__(self):
         return f' Покемон: {self.pokemon.title} Широта: {self.lat} Долгота: {self.lon}'
